9bf4de734ff9d3746b4c0ba6e84a40b371b9d677
kuriv <kuri.personal@gmail.com>
2019-12-08 22:25:02 +0800

    New update

---
+++ tests/Behavioral/TemplateMethod/TemplateMethodTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace PHPDesignPatterns\Behavioral\TemplateMethod;
+
+use PHPUnit\Framework\TestCase;
+
+class TemplateMethodTest extends TestCase
+{
+    public function testCanGetScheduleOfPeople()
+    {
+        $people = new People;
+        $people->process();
+        $this->assertEquals(
+            ['Go to work', 'Date with girlfriend', 'Go to sleep'],
+            $people->getSchedule()
+        );
+    }
+
+    public function testCanGetScheduleOfProgrammer()
+    {
+        $programmer = new Programmer;
+        $programmer->process();
+        $this->assertEquals(
+            ['Go to work', 'Programmer has no girlfriend', 'Go to sleep'],
+            $programmer->getSchedule()
+        );
+    }
+}
