bb49f6232747b360dcd817f9d405230c14453d4a
kuriv <kuri.personal@gmail.com>
2019-12-13 23:52:38 +0800

    New update

---
+++ src/Behavioral/Observer/UserObserver.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Kuriv\PHPDesignPatterns\Behavioral\Observer;
+
+use SplObserver;
+use SplSubject;
+
+class UserObserver implements SplObserver
+{
+    /**
+     * Store the changed users.
+     *
+     * @var array
+     */
+    private $changedUsers = [];
+
+    /**
+     * Update the property.
+     *
+     * @param  SplSubject $subject
+     * @return void
+     */
+    public function update(SplSubject $subject)
+    {
+        $this->changedUsers[] = clone $subject;
+    }
+
+    /**
+     * Get all changed users.
+     *
+     * @param  void
+     * @return array
+     */
+    public function getChangedUsers(): array
+    {
+        return $this->changedUsers;
+    }
+}
